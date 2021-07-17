c3ae4e90470862f1df5f5bbb6d3c46d3f49c8e03
Patrick Shaw <mail@patrickshaw.me>
2021-07-17 19:43:31 +1000

    Added powerlevel10k logic

--- index.sh
+++ index.sh
@@ -1,3 +1,5 @@
-source ./zinit.sh
-source ./docker-aliases.sh
-source ./git-aliases.sh
+source $(dirname $0)/powerlevel10k.sh
+
+source $(dirname $0)/zinit.sh
+source $(dirname $0)/docker-aliases.sh
+source $(dirname $0)/git-aliases.sh

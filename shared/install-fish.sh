a0683b1258e65a16b785f44a802b3991ea326734
Patrick Shaw <pshaw@atlassian.com>
2021-08-01 18:10:51 +1000

    Fixed install-fish.sh not running properly

--- shared/install-fish.sh
+++ shared/install-fish.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env fish
+#!/usr/bin/env bash
 sudo sh -c 'echo /usr/local/bin/fish >> /etc/shells'
 chsh -s /usr/local/bin/fish
 
-(status dirname)/../install-fisher.sh
+fish $(dirname $0)/../install-fisher.sh
 
 fisher install ilancosman/tide
 fisher install jorgebucaran/nvm.fish

8b680eb4a1afc347a632bebfa65c139219023754
Patrick Shaw <pshaw@atlassian.com>
2021-08-01 17:39:25 +1000

    Fixed up aliasing for fish

---
+++ shared/aliases/docker-aliases.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+alias stop-all-containers="$(dirname $0)/docker/stop-all-containers.sh"
+alias remove-all-containers="$(dirname $0)/docker/remove-all-containers.sh"
+alias remove-all-images="$(dirname $0)/docker/remove-all-images.sh"
+	
---
+++ shared/aliases/git-aliases.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+alias remove-branches-already-merged-to-master="$(dirname $0)/git/remove-branches-already-merged-to-master.sh"

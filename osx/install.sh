e76f6c628db75d2058aee2adb78196196a76a3d3
Patrick Shaw <mail@patrickshaw.me>
2021-08-04 13:39:34 +1000

    Added neovim

--- osx/install.sh
+++ osx/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-brew reinstall zoxide exa git-delta bat fzf fish
+brew reinstall zoxide exa git-delta bat fzf fish neovim
 
 ../shared/install-fish.fish
--- void-linux/install.sh
+++ void-linux/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo xbps-install -Su -y zoxide exa delta bat fzf fish-shell 
+sudo xbps-install -Su -y zoxide exa delta bat fzf fish-shell neovim
 
 $(dirname $0)/../shared/install-fish.fish

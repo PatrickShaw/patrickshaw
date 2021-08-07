011008d69d0065df67658e3d2096bc5b251c1aaf
Patrick Shaw <mail@patrickshaw.me>
2021-08-08 01:14:18 +1000

    Now autostarting services

--- void-linux/config.fish
+++ void-linux/config.fish
@@ -1,2 +1,9 @@
 source (status dirname)/../shared/config.fish
 alias code="code-oss"
+
+if status is-login
+    sway --my-next-gpu-wont-be-nvidia &
+    pipewire &
+    pipewire-pulse &
+    pipewire-media-session &
+end

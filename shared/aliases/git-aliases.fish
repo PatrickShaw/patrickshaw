6914c8aa894b2f58a6a26f712f3ac11efec6045b
Patrick Shaw <mail@patrickshaw.me>
2021-08-30 22:29:27 +1000

    Added git push --setup-upstream ... alias

--- shared/aliases/git-aliases.fish
+++ shared/aliases/git-aliases.fish
@@ -1,2 +1,3 @@
 #!/usr/bin/env fish
 alias remove-branches-already-merged-to-master="git branch --merged master |grep -v -e master| xargs -n 1 git branch -d"
+alias gp="git push --set-upstream (git remote show) (git branch --show-current)"

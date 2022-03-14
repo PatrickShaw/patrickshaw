28bcdc08d3f3cd0ab1239b75b86ed5691ca251b4
Patrick Shaw <mail@patrickshaw.me>
2022-03-15 02:43:27 +1100

    temp

--- void-linux/config.fish
+++ void-linux/config.fish
@@ -3,9 +3,18 @@ alias code="code-oss"
 
 fish_add_path $HOME/.cargo/bin
 
-if status is-login
-    sway --my-next-gpu-wont-be-nvidia &
-    pipewire &
-    pipewire-pulse &
-    pipewire-media-session &
+if status is-login 
+
+    #if test -z "$XDG_RUNTIME_DIR"
+    #  set runtime_dir "/tmp/"(id -u)"-xdg-runtime-dir"
+    #  mkdir "$runtime_dir"
+    #  chmod 0700 "$runtime_dir"
+    #  set -gx XDG_RUNTIME_DIR $runtime_dir       
+    #end
+
+    #sway --my-next-gpu-wont-be-nvidia &
+
+    dbus-launch pipewire &
+    dbus-launch pipewire-pulse &
+    dbus-launch pipewire-media-session &
 end

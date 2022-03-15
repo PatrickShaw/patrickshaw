e1cfcfe1f12e79d5bf8b057394bbedb7dbebdfd0
Patrick Shaw <mail@patrickshaw.me>
2022-03-15 23:14:35 +1100

    gp alias -> gpu. gp now just does git push. Added gc

--- personal/dotfiles/shared/aliases/docker-aliases.fish
+++ personal/dotfiles/shared/aliases/docker-aliases.fish
@@ -2,4 +2,3 @@
 alias stop-all-containers="docker stop (docker ps -q)"
 alias remove-all-containers="docker rm (docker container ls -aq)"
 alias remove-all-images="docker rmi (docker images -aq)"
-	
--- personal/dotfiles/shared/aliases/git-aliases.fish
+++ personal/dotfiles/shared/aliases/git-aliases.fish
@@ -1,3 +1,5 @@
 #!/usr/bin/env fish
 alias remove-branches-already-merged-to-master="git branch --merged master |grep -v -e master| xargs -n 1 git branch -d"
-alias gp="git push --set-upstream (git remote show) (git branch --show-current)"
+alias gpu="git push --set-upstream (git remote show) (git branch --show-current)"
+alias gp="git push"
+alias gc="git add . && git commit -m $0"

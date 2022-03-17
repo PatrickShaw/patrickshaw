e89f9b4dd94f7d9b8546a06609a9d9ccd9e498bc
Patrick Shaw <mail@patrickshaw.me>
2022-03-17 18:32:36 +1100

    Temp

--- personal/dotfiles/shared/install-fish.fish
+++ personal/dotfiles/shared/install-fish.fish
@@ -3,6 +3,6 @@ set fish_path (which fish)
 sudo sh -c "echo $fish_path >> /etc/shells"
 chsh -s $fish_path
 source (status dirname)/../install-fisher.fish
-fisher install jorgebucaran/fisher
+# fisher install jorgebucaran/fisher
 
-fisher install ilancosman/tide FabioAntunes/fish-nvm edc/bass
+fisher install ilancosman/tide@v5.0.1 FabioAntunes/fish-nvm edc/bass

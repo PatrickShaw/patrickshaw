afc414b6a865370dd8a88424f6b8bbbef13058f2
Patrick <mail@patrickshaw.me>
2022-03-18 22:31:21 +1100

    No longer forcing fish to become defualt shell

--- personal/dotfiles/shared/install-fish.fish
+++ personal/dotfiles/shared/install-fish.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 set fish_path (which fish)
 sudo sh -c "echo $fish_path >> /etc/shells"
-chsh -s $fish_path
+# chsh -s $fish_path
 source (status dirname)/../install-fisher.fish
 # fisher install jorgebucaran/fisher
 

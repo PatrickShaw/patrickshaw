4df164b7cd3cf5576064fb5c3633bab90ecf4699
Patrick Shaw <mail@patrickshaw.me>
2022-03-23 08:19:22 +1100

    Switched pkgs to nixpkgs-21.11-darwin

--- personal/dotfiles/nix/shared/configuration.nix
+++ personal/dotfiles/nix/shared/configuration.nix
@@ -1,5 +1,10 @@
 { pkgs, programs, ... }:
 let 
+  stable = import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/nixpkgs-21.11-darwin.tar.gz) {
+    config = {
+      allowUnfree = true;
+    };
+  };
   sharedAliases = {
     ls = "exa";
     cd = "z";
@@ -11,7 +16,7 @@ in
     nixpkgs.config.allowUnfree = true;
     nixpkgs.config.allowUnfreePredicate = (pkg: true);
  
-    environment.systemPackages = import ./apps.nix { inherit pkgs; };
+    environment.systemPackages = import ./apps.nix { pkgs = stable.pkgs; };
 
     services.nix-daemon.enable = true;
 

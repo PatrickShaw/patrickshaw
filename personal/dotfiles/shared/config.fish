d0733f0a389b6584b0807a019a2a4c536b49d7f1
Patrick Shaw <mail@patrickshaw.me>
2022-03-23 04:13:31 +1100

    Removed program-aliases.fish in favour of Nix shellAliases

--- personal/dotfiles/shared/config.fish
+++ personal/dotfiles/shared/config.fish
@@ -5,8 +5,6 @@ eval (ssh-agent -c) &> /dev/null
 source (status dirname)/aliases/docker-aliases.fish
 source (status dirname)/aliases/git-aliases.fish
 
-source (status dirname)/aliases/program-aliases.fish
-
 bind \cW backward-kill-word
 
 if status is-interactive

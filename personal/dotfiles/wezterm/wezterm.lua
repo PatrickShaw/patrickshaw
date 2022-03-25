881ec916c39bbcc2d93cacac470649cbaa3a5628
Patrick <mail@patrickshaw.me>
2022-03-26 04:24:06 +1100

    Added basic wezterm configs

---
+++ personal/dotfiles/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local w = require 'wezterm';
+
+return {
+  font = w.font("JetBrains Mono"),
+  hide_tab_bar_if_only_one_tab = true,
+  enable_scroll_bar = true,
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+  font_size = 11,
+  color_scheme = "Dark+",
+};

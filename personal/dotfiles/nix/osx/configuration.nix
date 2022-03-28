8b849fe260028f2fa1d59506c53ffce44d8f9f7c
Patrick Shaw <mail@patrickshaw.me>
2022-03-29 00:02:03 +1100

    Tied OSX nix config to unstable

--- personal/dotfiles/nix/osx/configuration.nix
+++ personal/dotfiles/nix/osx/configuration.nix
@@ -1,18 +1,38 @@
-{ config, pkgs, environment, ... }:
+{ config, pkgs, programs, environment, ... }:
+let
+  stable-pkgs = import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/nixpkgs-21.11-darwin.tar.gz) {
+    config = {
+      allowUnfree = true;
+    };
+  };
 
+  unstable-pkgs = import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz) {
+    config = {
+      allowUnfree = true;
+    };
+  };
+  default-pkgs = unstable-pkgs;
+
+  shared-configuration = import ../shared/configuration.nix {
+    inherit programs;
+    pkgs = default-pkgs;
+  };
+in
 {
+  # nix.package = default;
+  # nixpkgs.config.pkgs = default;
   imports = [
-    ../shared/configuration.nix
+    shared-configuration
   ];
   
   services.nix-daemon.enable = true;
-  environment.systemPackages = import ./apps.nix { inherit pkgs; };
+  environment.systemPackages = import ./apps.nix { pkgs = default-pkgs; };
 
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.allowUnfreePredicate = (pkg: true);
   
   fonts = {
     enableFontDir = true;
-    fonts = import ../shared/font-pkgs.nix { inherit pkgs; };
+    fonts = import ../shared/font-pkgs.nix { pkgs = default-pkgs; };
  };
 }

a460d93b0d02eb2d97dfec09ed68a7b75e0d602d
Patrick Shaw <pshaw@atlassian.com>
2022-06-29 19:10:43 +1000

    Upgraded stable

--- personal/dotfiles/nix/osx/stable-pkgs.nix
+++ personal/dotfiles/nix/osx/stable-pkgs.nix
@@ -1,6 +1,6 @@
 {}:
 let
-    pkgsModule = import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/nixpkgs-21.11-darwin.tar.gz);
+    pkgsModule = import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/nixpkgs-22.05-darwin.tar.gz);
 in
     pkgsModule {
       config = {

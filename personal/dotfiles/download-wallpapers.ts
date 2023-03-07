f79b21c77b2d44e629a616531d6d27ef66afc2f2
Patrick Shaw <mail@patrickshaw.me>
2023-03-08 00:44:23 +1100

    Better retry logic and caching for download-wallpapers.ts script

--- personal/dotfiles/download-wallpapers.ts
+++ personal/dotfiles/download-wallpapers.ts
@@ -1,30 +1,47 @@
 #!/usr/bin/env deno
 import { writableStreamFromWriter } from "https://deno.land/std@0.128.0/streams/mod.ts";
 import { extname } from "https://deno.land/std@0.128.0/path/mod.ts";
+import { existsSync } from "https://deno.land/std/fs/mod.ts";
 
 await Deno.mkdir("./wallpapers/wallhaven", { recursive: true });
 const text = await Deno.readTextFile('./wallhaven-backgrounds.txt');
-const ids = text.split("\n").filter((line) => /\w/.test(line));
-
+const ids = new Set(text.split("\n").filter((line) => /\w/.test(line)));
 for (const id of ids) {
     try {
         // See: https://wallhaven.cc/help/api
         const url = `https://wallhaven.cc/api/v1/w/${id}`;
-        const response =  await fetch(url);
-        const json: any = await response.json();
-        const data=  json.data;
-
-        const ext = extname(data.path);
-        const imagePath = `./wallpapers/wallhaven/${id}${ext}`;
-        const imageFile = await Deno.open(imagePath, { write: true, create: true });
-        const imageResponse = await fetch(data.path);
-        const imageWriteStream = writableStreamFromWriter(imageFile);
-        await imageResponse.body!.pipeTo(imageWriteStream);
-        console.log(`Saved wallpaper: ${imagePath}`);
+        let retries = 0;
+        while(true) {
+            const response =  await fetch(url);
+            if (response.status === 429) {
+                const timeout = 500 * (2 ** retries);
+                console.log(`too many requests, waiting for ${timeout}ms`);
+                await new Promise(resolve => setTimeout(resolve, ));
+                retries++;
+                continue;
+            }
+            if (!response.headers.get('content-type')?.includes('application/json')) {
+                console.log(`Received unexpected content type ${response.headers.get('content-type')}. Response was: `, response.status, response.statusText, await response.text());
+                break;
+            }
+            const json: any = await response.json();
+            const data=  json.data;
+    
+            const ext = extname(data.path);
+            const imagePath = `./wallpapers/wallhaven/${id}${ext}`;
+    
+            if (existsSync(imagePath)) {
+                console.log(`Skipping ${id} because it already exists as ${imagePath}`)
+                break;
+            }
+            
+            const imageFile = await Deno.open(imagePath, { write: true, create: true });
+            const imageResponse = await fetch(data.path);
+            const imageWriteStream = writableStreamFromWriter(imageFile);
+            await imageResponse.body!.pipeTo(imageWriteStream);
+            console.log(`Saved wallpaper: ${imagePath}`);
+        }
     } catch(err) {
         console.error(`Failed to retrieve ${id}`, err);
     }
-
-    // Have to wait a little bit. Wallhaven doesn't like too many requests at once (you'll get an HTTP 429 error)
-    await new Promise(resolve => setTimeout(resolve, 1000));
 }

36148297624ffebfd23674932390895fed602663
Patrick Shaw <mail@patrickshaw.me>
2023-03-08 00:47:53 +1100

    Disabled ALSA (for now)

--- personal/dotfiles/nix/linux/configuration.nix
+++ personal/dotfiles/nix/linux/configuration.nix
@@ -77,8 +77,8 @@ in {
 
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    #alsa.enable = true;
+    #alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
     "media-session".enable = false;

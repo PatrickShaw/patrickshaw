7790507ea9fe294ef5021578c55a617f36dcd4a3
Patrick Shaw <mail@patrickshaw.me>
2023-03-27 11:43:51 +1100

    Added caching

--- personal/dotfiles/nix/linux/configuration.nix
+++ personal/dotfiles/nix/linux/configuration.nix
@@ -24,6 +24,25 @@ in
       shared-configuration
       hyprland.nixosModules.default
   ];
+
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos.org/"
+
+      # See: https://wiki.hyprland.org/Nix/Cachix/
+      "https://hyprland.cachix.org"
+
+      # See: https://github.com/nix-community/nixpkgs-wayland#binary-cache
+      "https://nixpkgs-wayland.cachix.org"
+
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+    ];
+  };
   
   programs.hyprland = {
     enable = true;

14061050b90faadf032c1d9b870f3ed3c503b8fc
Patrick Shaw <mail@patrickshaw.me>
2023-05-29 22:58:41 +1000

    Re-order

--- personal/dotfiles/nix/flake.nix
+++ personal/dotfiles/nix/flake.nix
@@ -9,13 +9,13 @@
     extra-substituters = [
       "https://cache.nixos.org/"
 
-      # See: https://wiki.hyprland.org/Nix/Cachix/
-      "https://hyprland.cachix.org"
-
       # See: https://github.com/nix-community/nixpkgs-wayland#binary-cache
       "https://nixpkgs-wayland.cachix.org"
 
       "https://nix-community.cachix.org"
+
+      # See: https://wiki.hyprland.org/Nix/Cachix/
+      "https://hyprland.cachix.org"
     ];
     extra-trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="

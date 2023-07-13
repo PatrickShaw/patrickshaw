4234647771ec2e6f196d0cddffc766b45dc75980
Patrick Shaw <mail@patrickshaw.me>
2023-07-13 19:55:32 +1000

    Folder rename

--- personal/code-snippets/desktop/brightness-change.sh
+++ personal/code-snippets/desktop/brightness-change.sh
@@ -4,7 +4,6 @@ MAX="$(brightnessctl -m m)"
 CURRENT="$(brightnessctl -m g)"
 
 NEXT="$(frawk "BEGIN { print int($CURRENT $1 $MAX * 0.1 + 0.5) }")"
-echo $NEXT
 
 brightnessctl s $NEXT
 
---
+++ personal/code-snippets/desktop/search-launcher.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+killall .wofi-wrapped || wofi --show drun --allow-images --allow-markup --style $HOME/.config/wofi/styles.css

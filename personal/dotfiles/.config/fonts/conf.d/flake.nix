4fd75eb31bc01a52197ca0ca4b759b478877122f
Patrick Shaw <mail@patrickshaw.me>
2023-08-04 23:49:31 +1000

    More font tweaks

--- personal/dotfiles/.config/fonts/conf.d/flake.nix
+++ personal/dotfiles/.config/fonts/conf.d/flake.nix
@@ -1,6 +1,24 @@
 {
   outputs = { ... }: {
-    nixosModules.default = { ... }: {
+    nixosModules.default = { pkgs, ... }: {
+      environment.systemPackages = [
+        # too big: pkgs.nerdfonts
+
+        # See: https://dribbble.com/stories/2021/04/26/web-design-data-fonts
+        # See: https://jichu4n.com/posts/the-most-popular-fonts-on-the-web-a-study/
+        # "Times New Roman, Arial, and Courier New" replacement
+        pkgs.liberation_ttf
+        # "Arial Narrow" replacement
+        pkgs.liberation-sans-narrow
+        pkgs.helvetica-neue-lt-std
+        pkgs.roboto
+        pkgs.open-sans
+        # Georgia replacement
+        pkgs.gelasio
+        pkgs.font-awesome
+        pkgs.vistafonts
+
+      ];
       fonts = {
         fontconfig = {
           localConf = builtins.readFile ./default-font.conf;

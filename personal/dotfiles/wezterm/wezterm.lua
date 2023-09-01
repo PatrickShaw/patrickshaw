4608b21a5f001c4687f9fb55bf0eda5e32fa90bf
Patrick Shaw <mail@patrickshaw.me>
2023-09-02 00:51:18 +1000

    Wezterm tweaks

--- personal/dotfiles/wezterm/wezterm.lua
+++ personal/dotfiles/wezterm/wezterm.lua
@@ -3,9 +3,11 @@ return {
   font = w.font("JetBrains Mono", {
     weight = "Light"
   }),
+  check_for_updates = false,
   hide_tab_bar_if_only_one_tab = true,
-  enable_scroll_bar = true,
+  enable_scroll_bar = false,
   enable_wayland = true,
+  color_scheme   = "followSystem",
   window_padding = {
     left = "5%",
     right = "5%",
@@ -13,10 +15,9 @@ return {
     bottom = 20,
   },
   font_size = 11,
-  color_scheme = "Dracula+",
+  --color_scheme = "Dracula+",
   colors={
-    --background='rgba(0,0,0,0.9)',
-    background='rgb(30, 30, 30)',
+    background='rgba(18,19,17,0.8)',
     scrollbar_thumb = 'rgba(255, 255, 255, 0.01)',
   },
   scrollback_lines = 10000,

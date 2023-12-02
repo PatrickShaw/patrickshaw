8eb085cdb9d0585de4a3b60c54f619fab99ac565
Patrick Shaw <mail@patrickshaw.me>
2023-12-03 01:45:33 +1100

    Background switcher now works beyond workspace 9

--- personal/dotfiles/hypr/watch-background.sh
+++ personal/dotfiles/hypr/watch-background.sh
@@ -12,8 +12,6 @@ activeWorkspace() {
 
   declare -A idToImg
   
-  echo switch
-
   idToImg["1"]="wyqdvp.jpg"
   idToImg["2"]="96kq3x.png"
   idToImg["3"]="k9p8l6.jpg"
@@ -25,8 +23,11 @@ activeWorkspace() {
   idToImg["9"]="39ode6.png"
   idToImg["0"]="eyw8yr.jpg"
   
-  IMG_FILENAME=$(echo ${idToImg["$ID"]})
+  INDEX=$(frawk "BEGIN { printf $ID % 10 }")
+  
+  IMG_FILENAME=$(echo ${idToImg["$INDEX"]})
   PREV_ID=$ID
+  
   swww img --transition-type=simple --transition-fps=60 --transition-step=48 $HOME/wallpapers/wallhaven/$IMG_FILENAME
 }
 

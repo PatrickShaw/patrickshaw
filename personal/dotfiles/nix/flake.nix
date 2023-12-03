62d2b5a4a96d6544b5f3b685e9ee36b02547b0a8
Patrick Shaw <mail@patrickshaw.me>
2023-12-04 00:06:33 +1100

    Solved GTK woes

--- personal/dotfiles/nix/flake.nix
+++ personal/dotfiles/nix/flake.nix
@@ -200,6 +200,26 @@
           '';
         };
 
+
+        # See https://nixos.wiki/wiki/Sway
+        configure-gtk = pkgs.writeTextFile {
+          name = "configure-gtk";
+          destination = "/bin/configure-gtk";
+          executable = true;
+          text = let
+            schema = pkgs.gsettings-desktop-schemas;
+            datadir = "${schema}/share/gsettings-schemas/${schema.name}";
+          in ''
+            export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+            gnome_schema=org.gnome.desktop.interface
+
+            gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+            gsettings set org.gnome.desktop.interface gtk-theme 'Orchis-Green'
+            # Add for good measure. See: https://wiki.hyprland.org/FAQ/#gtk-settings-no-work--whatever
+            gsettings set org.gnome.desktop.interface cursor-theme 'phinger-cursors'
+            gsettings set org.gnome.desktop.interface cursor-blink false
+          '';
+        };
       in {
         imports = [
             shared-configuration
@@ -377,7 +397,7 @@
 
 
           # For whatever reason nautilus won't respect my theme without this
-          GTK_THEME = "Orchis-Green:dark";
+          # GTK_THEME = "Orchis-Green:dark";
 
           MOZ_ENABLE_WAYLAND = "1";
 

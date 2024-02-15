a15d4cfe29ba347f1db3ccba1355ceaa0cd9aaf5
Patrick Shaw <mail@patrickshaw.me>
2024-02-15 22:43:58 +1100

    Increase temp file size

--- personal/dotfiles/nix/linux/hardware/fs/impermanence.nix
+++ personal/dotfiles/nix/linux/hardware/fs/impermanence.nix
@@ -13,7 +13,7 @@ in
     "/" = {
       device = "none";
       fsType = "tmpfs";
-      options = [ "defaults" "size=4G" "mode=755" ];
+      options = [ "defaults" "size=8G" "mode=755" ];
       neededForBoot = true;
     };
 

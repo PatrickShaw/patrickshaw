acd86cd6409d3c29bd5167ba9a898d6b38ff301a
Patrick Shaw <mail@patrickshaw.me>
2024-07-17 02:20:25 +1000

    Added packages and EDITOR settings for barebones:

--- personal/dotfiles/nix/flake.nix
+++ personal/dotfiles/nix/flake.nix
@@ -203,6 +203,13 @@
         ];
       };
       barebones = { lib, ... }:  {
+        EDITOR = "nvim";
+        environment.systemPackages = [
+          lm_sensors
+          neovim # TODO: Find a minmal set of nvim packages
+          git
+        ];
+
         # Be careful: If you do not add these flags to scripts that when running nix commands, these commands will fail for those who don't enable these flags via configurations
         nix.settings.experimental-features = [ "flakes" "nix-command" ];
 

aa036245a1ad3aa51288b5c986c1e74a9c2dcb30
Patrick Shaw <pshaw@atlassian.com>
2025-08-05 02:20:33 +1000

    No more nix-gaming no more binary cache

--- personal/dotfiles/nix/flake.nix
+++ personal/dotfiles/nix/flake.nix
@@ -22,35 +22,12 @@
 
 
 
-    nix-gaming.url = "github:fufexan/nix-gaming";
+    # nix-gaming.url = "github:fufexan/nix-gaming";
     # nix-gaming.inputs.nixpkgs.follows = "nixpkgs";
 
     # nice-overlays.url = "./linux/nice-overlays";
   };
   nixConfig = {
-    extra-substituters = [
-      "https://cache.nixos.org?priority=1"
-
-      "https://nix-gaming.cachix.org"
-
-      # See: https://wiki.hyprland.org/Nix/Cachix/
-      # "https://hyprland.cachix.org"
-
-      # See: https://github.com/nix-community/nixpkgs-wayland#binary-cache
-      # "https://nixpkgs-wayland.cachix.org"
-
-      "https://nix-community.cachix.org"
-
-      # "https://helix.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      # "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-      # "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
-    ];
   };
   outputs = { self, ... }@inputs: 
   {
@@ -330,7 +307,6 @@
             self.nixosModules.base
             self.nixosModules.text-to-speech
             ./shared/binary-caching.nix
-            inputs.nix-gaming.nixosModules.pipewireLowLatency
             # Merged to upstream (see: https://github.com/NixOS/nixpkgs/pull/293564), and so the following is no longer needed.
             # inputs.nix-gaming.nixosModules.steamCompat
         ];
--- personal/dotfiles/nix/shared/binary-caching.nix
+++ personal/dotfiles/nix/shared/binary-caching.nix
@@ -1,27 +1,4 @@
 {
   nix.settings = {
-    extra-substituters = [
-      "https://cache.nixos.org?priority=1"
-
-      "https://nix-gaming.cachix.org"
-
-      # See: https://wiki.hyprland.org/Nix/Cachix/
-      # "https://hyprland.cachix.org"
-
-      # See: https://github.com/nix-community/nixpkgs-wayland#binary-cache
-      # "https://nixpkgs-wayland.cachix.org"
-
-      "https://nix-community.cachix.org"
-
-      # "https://helix.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      # "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-      # "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
-    ];
   };
 }
